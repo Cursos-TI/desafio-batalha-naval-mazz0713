5125b7c4bf17a083c185b49f22487095e804e4a7
Ilmar Victor Farias de Araújo <mazz0713@gmail.com>
2025-08-26 12:23:52 +0000

    batalhaNaval novo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,97 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
 
-int main() {
+    // Segunda carta
 
-    printf("desafio base naval");
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("primeira carta\n");
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("Estado (A a H):");
+    scanf("%c", &estado1);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    printf("Codigo (ex: A01): ");
+    scanf("%s", &codigo1);
+
+    printf("Nome da cidade: ");
+    scanf("%s", &cidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (Km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB:  ");
+    scanf("%f", &pib1);
+
+    printf("Pontos Turisticos:  ");
+    scanf("%d", &pontos1);
+
+    printf("\n");
+
+    //segunda Carta
+
+    printf(" Segunda Carta \n");
+
+    printf("Estado (A a H): ");
+    scanf("%c", &estado2);
+
+    printf("Codigo (ex: B02): ");
+    scanf("%s", &codigo2);
+
+    printf("Nome da Cidade:  ");
+    scanf("%s", cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Area (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontos2);
+
+    printf("\n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", &estado1);
+    printf("Codigo: %s\n", &codigo1);
+    printf("Nome da cidade: %s\n", &cidade1);
+    printf("População: %d\n", &populacao1);
+    printf("Area: %.2f Km²\n", &area1);
+    printf("PIB: %.2f Bilhoes de reais\n", &pib1);
+    printf("Numero de Pontos Turisticos: %d\n", &pontos1);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("\n");
+
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", &estado2);
+    printf("Codigo: %s\n", &codigo2);
+    printf("Nomde da Cidade: %s\n", &cidade2);
+    printf("População: %d\n", &populacao2);
+    printf("Area: %.2f Km²\n", area2);
+    printf("PIB: %.2f Bilhoes de reais\n", &pib2);
+    printf("Numero de Pontos Turisticos: %d\n", &pontos2);
 
     return 0;
+
+
 }
